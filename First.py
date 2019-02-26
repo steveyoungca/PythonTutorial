4b9721a7d2f4687d68952a3d64b68316193ebe8a
steveyoungca <steveyoungca@users.noreply.github.com>
2019-02-25 20:51:42 -0500

    Our First Commit

---
+++ First.py
@@ -0,0 +1,23 @@
+##  Python SCRIPT: Apple.py
+## 
+##  Description:  Sample Code for a Hello World
+##                
+## 
+##  Parameters : None
+##
+##  License:  
+##
+##  Repository: 
+## 
+##  Documentation on the  package 
+
+##
+##
+##  Date				    Developer		  	Action
+##  ---------------------------------------------------------------------
+##  Jan 20, 2019    		Steve Young			Inital development 
+## 
+## 
+## 
+## 
+## 
